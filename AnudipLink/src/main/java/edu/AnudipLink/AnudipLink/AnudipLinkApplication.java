69c599bf3c4c880f724368c105adaada132d5348
potphodeaditi <121276947+potphodeaditi@users.noreply.github.com>
2023-03-09 20:08:42 +0530

    Add files via upload

---
+++ AnudipLink/src/main/java/edu/AnudipLink/AnudipLink/AnudipLinkApplication.java
@@ -0,0 +1,14 @@
+package edu.AnudipLink.AnudipLink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnudipLinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnudipLinkApplication.class, args);
+
+	}
+
+}
